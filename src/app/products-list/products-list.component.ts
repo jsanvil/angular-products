8e0302f74a2ef3c01c8ced89a93f8951cb218608
Jacinto Sanchez <j.sanchezvilla@edu.gva.es>
2024-01-10 10:18:01 +0100

    updated products

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -15,18 +15,26 @@ export class ProductsListComponent {
   products: Product[] = [
     {
       id: 1,
-      description: 'SSD hard drive',
-      available: '2016-10-03',
-      price: 75,
+      description: 'WD BLACK SN770 2TB NVMe SSD',
+      available: '2023-10-03',
+      price: 115,
       imageUrl: 'assets/ssd.jpg',
       rating: 5
     }, {
       id: 2,
-      description: 'LGA1151 Motherboard',
-      available: '2016-09-15',
-      price: 96.95,
+      description: 'MSI MPG B550 GAMING PLUS ',
+      available: '2023-09-15',
+      price: 139.90,
       imageUrl: 'assets/motherboard.jpg',
       rating: 4
+    },
+    {
+      id: 3,
+      description: 'Kingston FURY Beast DDR4 3200 MHz 16GB 2x8GB CL16',
+      available: '2023-11-10',
+      price: 42.95,
+      imageUrl: 'assets/ram.jpg',
+      rating: 3
     }
   ];
 
